a995d1b4854918a20011ace8ed82ad17829096da
Alvinhu182 <alvinhu@hotmail.com>
2023-06-27 15:44:59 -0300

    primeiras estilizações feitas com o bootstrap

--- cadastrar.php
+++ cadastrar.php
@@ -1,13 +1,30 @@
-<h1> Cadastrar Usuarios </h1>
-<form method="POST" action="cadastrar_action.php">
-    <label>
-        Nome: <input type="text" name="nome"/>
-    </label>
-    <label>
-        Email: <input type="email" name="email"/>
-    </label>
-    <label>
-        Telefone: <input type="int" name="telefone"/>
-    </label>
-<input type="submit" value="Salvar"> 
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Cadastrar</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Cadastrar Usuários</h1>
+        <form method="POST" action="cadastrar_action.php">
+            <div class="mb-3">
+                <label for="nome" class="form-label">Nome:</label>
+                <input type="text" class="form-control" id="nome" name="nome">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input type="email" class="form-control" id="email" name="email">
+            </div>
+            <div class="mb-3">
+                <label for="telefone" class="form-label">Telefone:</label>
+                <input type="text" class="form-control" id="telefone" name="telefone">
+            </div>
+            <button type="submit" class="btn btn-primary">Salvar</button>
+            <a class="btn btn-success" href="index.php">Voltar</a>
+        </form>
+    </div>
+</body>
+</html>
--- editar.php
+++ editar.php
@@ -1,35 +1,52 @@
-<?php 
-require 'config.php';
-
-$usuario = [];
-$id = filter_input(INPUT_GET, 'id');
-if ($id) {
-    $sql = $pdo->prepare("SELECT * FROM usuario WHERE id = :id");
-    $sql->bindValue(':id',$id);
-    $sql->execute();
-
-    if($sql->rowCount() > 0 ){
-        $usuario = $sql->fetch(PDO::FETCH_ASSOC);
-    } else {
-        header("Location: index.php");
-        exit;
-    }
-} else {
-    header("Location: index.php");
-}
-?>
-
-<h1> Editar Usuario </h1>
-<form method="POST" action="editar_action.php">
-    <input type="hidden" name="id" value="<?=$usuario['id'];?>"/>
-    <label>
-        Nome: <input type="text" name="nome" value="<?=$usuario['nome'];?>"/>
-    </label>
-    <label>
-        Email: <input type="email" name="email" value="<?=$usuario['email'];?>" />
-    </label>
-    <label>
-        Telefone: <input type="int" name="telefone" value="<?=$usuario['telefone'];?>" />
-    </label>
-    <input type="submit" value="Atualizar"/>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Editar</title>
+</head>
+<body>
+    <div class="container">
+        <?php 
+        require 'config.php';
+        
+        $usuario = [];
+        $id = filter_input(INPUT_GET, 'id');
+        if ($id) {
+            $sql = $pdo->prepare("SELECT * FROM usuario WHERE id = :id");
+            $sql->bindValue(':id',$id);
+            $sql->execute();
+        
+            if($sql->rowCount() > 0 ){
+                $usuario = $sql->fetch(PDO::FETCH_ASSOC);
+            } else {
+                header("Location: index.php");
+                exit;
+            }
+        } else {
+            header("Location: index.php");
+        }
+        ?>
+        
+        <h1>Editar Usuário</h1>
+        <form method="POST" action="editar_action.php">
+            <input type="hidden" name="id" value="<?=$usuario['id'];?>"/>
+            <div class="mb-3">
+                <label for="nome" class="form-label">Nome:</label>
+                <input type="text" class="form-control" id="nome" name="nome" value="<?=$usuario['nome'];?>">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" value="<?=$usuario['email'];?>">
+            </div>
+            <div class="mb-3">
+                <label for="telefone" class="form-label">Telefone:</label>
+                <input type="text" class="form-control" id="telefone" name="telefone" value="<?=$usuario['telefone'];?>">
+            </div>
+            <button type="submit" class="btn btn-primary">Atualizar</button>
+            <a class="btn btn-success" href="index.php">Voltar</a>
+        </form>
+    </div>
+</body>
+</html>
